14fe4b125eb87e1b9f03131070fed944a266048c
Rahil <rahilmehta18@gmail.com>
2021-07-06 12:43:22 +0530

    upadated on 22-07-21

--- Number Guesser /number_guesser.py
+++ Number Guesser /number_guesser.py
@@ -1,21 +1,27 @@
+#importing the random library
 import random
 
+print("-----Let's play the Number Guessing game -----")
 
+#asking user the max number we wants to guess
 upper_limit = int(input("Enter the upper limit of the number: "))
 
+#creating the random number 
 random_number = random.randint(0,upper_limit)
 
-print(random_number) 
-
+#keeping th count the number of guess 
 total_guess = 0
 
 while True:
+    #adding 1 to total_guess everytime the loop runs
     total_guess +=1
+    #asking user to make a guess 
     user_guess = int(input("Make your Guess: "))
 
     if user_guess == random_number:
         print("Vola it's the correct guess !! ")
         break
+
     elif user_guess > random_number:
         print("your guess is greater than the number ")
 
@@ -23,4 +29,4 @@ while True:
         print("your guess is less then the number")
     
     
-print("you took total", total_guess,"to get the correct number")
+print("you took total", total_guess," guess to get the correct number")
